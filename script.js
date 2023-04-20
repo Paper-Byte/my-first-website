0aadfba375efa3a8738f2834472715d75beb645b
Noah <noahhughesph@gmail.com>
2023-04-20 16:06:59 -0400

    began building slider and button

--- script.js
+++
@@ -1,8 +0,0 @@
-const img = window.document.getElementsByClassName('mainpic')[0];
-img.addEventListener('mouseenter', () => {
-    img.classList.add('active');
-});
-img.addEventListener('mouseleave', () => {
-    img.classList.remove('active');
-});
-console.log(img);
